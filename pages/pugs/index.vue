ca28b08205fee6793b729755d9e2d63a96ddd93e
Sean Chen <Sean.Chen@leocorn.com>
2019-12-17 14:15:48 +0000

    the simple pug page.

---
+++ pages/pugs/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  h1.red Hello {{ name }}!
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'Pug World'
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+.red
+  color: red
+</style>
