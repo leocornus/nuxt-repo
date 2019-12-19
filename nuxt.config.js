4881c9a0ee5b28e9508ee6a678eb7dad7cf654d5
Sean Chen <sean.chen@leocorn.com>
2019-12-19 14:29:36 -0500

    using vuetify-module to load Vuetify in Nuxt.js application

---
+++ nuxt.config.js
@@ -0,0 +1,21 @@
+/**
+ * configuration for Nuxt.js
+ */
+export default {
+
+    buildModules: [
+        // load the nuxtjs vutify-module
+        // https://github.com/nuxt-community/vuetify-module
+        '@nuxtjs/vuetify'
+    ],
+
+    /**
+     * options to set up vuetify, details on page
+     *  - https://github.com/nuxt-community/vuetify-module
+     */
+    vuetify: {
+
+        // we could set up default assets here.
+        // By default, automatically handle Roboto font & Material Design Icons
+    }
+}
