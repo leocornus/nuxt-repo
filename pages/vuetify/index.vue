928a776d08454606051a73358a61e7fbfef1bca2
Sean Chen <sean.chen@leocorn.com>
2019-12-19 14:31:04 -0500

    The first vuetify page in a Nuxt.js application using pug

---
+++ pages/vuetify/index.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+v-container(grid-list-md text-xs-center)
+  v-layout(row wrap)
+    v-flex(xs12)
+      v-card(dark color="red")
+        v-card-text(class="px-0") {{message}}
+    v-flex(v-for="i in 12 * 20" :key="`b${i}`" xs1)
+      v-card(dark :color="`#${(i * 50).toString(16)}`")
+        v-card-text(class="px-0") {{i}}
+</template>
+
+<script>
+export default {
+  name: 'VuetifyScratchPad',
+
+  data () {
+    return {
+      message: '',
+    };
+  },
+
+  methods: {
+  },
+
+  created() {
+
+    this.message = "this.$localSettings.applicationName";
+  }
+}
+</script>
