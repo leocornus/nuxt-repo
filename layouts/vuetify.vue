e829973429415143b9f501d56e2188d6fca9b95a
Sean Chen <sean.chen@leocorn.com>
2019-12-19 17:12:01 -0500

    add the dense to make smaller nav bar and padless footer

--- layouts/vuetify.vue
+++ layouts/vuetify.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 // Simple layout for Vuetify playground
 v-app
-  v-app-bar(color='indigo' dark fixed app)
+  v-app-bar(color='indigo' dark fixed app dense)
     v-app-bar-nav-icon/
     v-toolbar-title Toolbar Grid Footer
     v-spacer
@@ -13,10 +13,10 @@ v-app
   v-content
     Nuxt/
 
-  v-footer(height="auto" color="indigo" app)
+  v-footer(color="indigo" app padless)
     v-layout(justify-center row wrap)
-      v-flex(py-3 text-xs-center white--text xs12)
-        | &copy;2019 - <strong>Leocornus LTD.</strong>
+      v-flex(py-3 text-center white--text xs12)
+        | &copy;{{new Date().getFullYear()}}- <strong>Leocornus LTD.</strong>
 </template>
 
 <script>
