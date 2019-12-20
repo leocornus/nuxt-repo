ab10e016ebc495e02fda0fe59b39442db0b00f49
Sean Chen <sean.chen@leocorn.com>
2019-12-20 07:02:00 -0500

    fix the defect of missing white space

--- layouts/vuetify.vue
+++ layouts/vuetify.vue
@@ -16,7 +16,7 @@ v-app
   v-footer(color="indigo" app padless)
     v-layout(justify-center row wrap)
       v-flex(py-3 text-center white--text xs12)
-        | &copy;{{new Date().getFullYear()}}- <strong>Leocornus LTD.</strong>
+        | &copy;{{new Date().getFullYear()}} - <strong>Leocornus LTD.</strong>
 </template>
 
 <script>
