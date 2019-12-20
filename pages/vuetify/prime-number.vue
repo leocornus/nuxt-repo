77479927b88572bc6cea0f2855c38c934ca4b89f
Sean Chen <sean.chen@leocorn.com>
2019-12-20 12:37:12 -0500

    update to use the component card

--- pages/vuetify/prime-number.vue
+++ pages/vuetify/prime-number.vue
@@ -18,26 +18,17 @@ v-container( grid-list-xs )
           |{{ resultMessage }}
 
     // The card for prime number list.
-    v-flex( xs12 sm4 )
-      v-toolbar( color="indigo" dark dense light )
-        v-toolbar-title Prime Numbers List
-      v-card
-        v-card-text
-          v-layout( row wrap text-center )
-            v-flex(
-              v-for="i in 200"
-              :key="`n${i}`"
-              xs1
-            )
-              v-card(
-                dark
-                :color='`${isPrime(i) ? "danger" : "success"}`'
-              )
-                v-card-text( class="px-0 py-1") {{ i }}
+    card-pns/
 </template>
 
 <script>
+import CardPrimeNumbers from '@/pages/vuetify/card-primenumbers.vue'
+
 export default {
+    components: {
+        'card-pns': CardPrimeNumbers
+    },
+
     layout: 'vuetify',
 
     name: 'prime-number',
@@ -47,13 +38,7 @@ export default {
 
             inputNumber: null,
 
-            loading: false,
-
-            primeNumbers: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-                41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
-                103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157,
-                163, 167, 173, 179, 181, 191, 193, 197, 199]
-
+            loading: false
         };
     },
 
@@ -73,18 +58,6 @@ export default {
         checkPrimeNumber: function(theNumber) {
 
             console.log(theNumber);
-        },
-
-        /**
-         * return true if the given number is a prime number.
-         */
-        isPrime: function(n) {
-
-            if(this.primeNumbers.includes(n)) {
-                return true;
-            } else {
-                return false;
-            }
         }
     },
 }
