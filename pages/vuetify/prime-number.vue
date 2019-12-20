a8bd23853a53659f78e10cccbe88c636f7a35222
Sean Chen <sean.chen@leocorn.com>
2019-12-20 07:08:58 -0500

    get started the prime number check page.

---
+++ pages/vuetify/prime-number.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  v-row
+    v-text-field( 
+      v-model='inputNumber' 
+      label="Give me a number:"
+      loading
+    )
+  v-layout
+    v-flex( xs12 sm4 )
+      v-toolbar( color="indigo" dark dense light wihte--text )
+        v-toolbar-title Prime Number
+      v-card
+        v-card-title( primary-title ) get ready
+
+</template>
+
+<script>
+export default {
+    layout: 'vuetify',
+
+    name: 'prime-number',
+
+    data () {
+        return {
+            inputNumber: '',
+        };
+    },
+
+    methods: {
+    },
+}
+</script>
