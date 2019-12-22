5cc106880a2d5b6b9605217631bf9b6d471f0a27
Sean Chen <sean.chen@leocorn.com>
2019-12-22 14:00:49 -0500

    implement the primality test algorithm

--- libs/PrimeNumber.js
+++ libs/PrimeNumber.js
@@ -6,7 +6,8 @@ let pn = {
     /**
      * Prime numbers up to 500
      */
-    primeNumbers500: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 
+    primeNumbers500: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+        53, 59, 61, 67, 
         71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 
         149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 
         227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 
@@ -14,13 +15,35 @@ let pn = {
         389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 
         467, 479, 487, 491, 499],
 
+    /**
+     * based on the algorithm from wikipedia: primality test.
+     * https://en.wikipedia.org/wiki/Primality_test
+     */
     isPrimeNumber: function(aNumber) {
 
-        if(this.primeNumbers500.includes(aNumber)) {
-            return true;
-        } else {
+        // check if it is less then 1
+        if (aNumber <= 3) {
+            return aNumber > 1;
+        } else if( (aNumber % 2 === 0) || (aNumber % 3 === 0) ) {
             return false;
         }
+
+        let i = 5;
+        while( i * i <= aNumber ) {
+            if( (aNumber % i === 0) || (aNumber % (i + 2) === 0) ) {
+                return false;
+            }
+
+            i = i + 6;
+        }
+
+        return true;
+
+        //if(this.primeNumbers500.includes(aNumber)) {
+        //    return true;
+        //} else {
+        //    return false;
+        //}
     }
 };
 
