3bed2d32dc08f73313dd7318e4756ba795526650
Sean Chen <sean.chen@leocorn.com>
2019-12-23 17:20:54 +0000

    set the router base for the production deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,20 @@
  */
 export default {
 
+    // set to Single Page application mode.
+    mode: 'spa',
+
     buildModules: [
         // load the nuxtjs vutify-module
         // https://github.com/nuxt-community/vuetify-module
         '@nuxtjs/vuetify'
     ],
+
+    router: {
+        // tweak the base, if we plan to deploy on a subfolder
+        // /demo/nuxt
+        base: process.env.NODE_ENV === 'dev' ? '/' : '/demo/nuxt/'
+    },
 
     /**
      * options to set up vuetify, details on page
