206cfc326030bce2d5d900b8161885b4d8e79324
Sean Chen <sean.chen@leocorn.com>
2019-12-29 11:51:54 -0500

    introduce the card for search settings.

---
+++ pages/solr/card-settings.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+v-card
+  v-card-title( class="headline" )
+    | Search settings
+
+  v-card-text
+    | we will have a comprehensive settings list here.
+
+  v-card-actions
+    v-spacer
+    v-btn( color="green darken-1" text
+      @click="cancelSettings"
+    ) cancel
+    v-btn( color="green darken-1" text
+      @click="saveSettings"
+    ) save
+
+</template>
+
+<script>
+
+export default {
+    name: "dialog-settings",
+
+    // set the properties for this component.
+    props: {
+        'cancelSettings': null,
+        'saveSettings': null
+    }
+}
+</script>
