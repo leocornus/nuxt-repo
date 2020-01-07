c4cc07a68fd8387369549ef643045d71d3c7e0e0
Sean Chen <sean.chen@leocorn.com>
2020-01-07 12:54:40 -0500

    get started the listing preview card

---
+++ pages/solr/card-listing-preview.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+v-card
+  v-card-title {{previewTitle}}
+  v-card-text {{previewDesc}}
+
+</template>
+
+<script>
+
+export default {
+    name: "listing-preview",
+
+    // set the properties for this component.
+    props: {
+        "doc": Object,
+        "index": Number,
+        // set the name for id field.
+        "idFieldName": {
+            type: String,
+            default: "id"
+        },
+        "thePage": Object
+    },
+
+    computed: {
+
+        /**
+         * preview title for each doc.
+         */
+        previewTitle() {
+
+            console.log(this.doc);
+            return this.doc.title;
+        },
+
+        /**
+         * preview title for each doc.
+         */
+        previewDesc() {
+
+            return this.doc.teaser;
+        }
+    },
+
+    data() {
+        return {};
+    }
+}
+</script>
