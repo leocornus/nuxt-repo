9e6fc138e5d759452013eec7012be3f149a5b46f
Sean Chen <sean.chen@leocorn.com>
2020-01-09 13:37:59 -0500

    add the component to show query filters

---
+++ pages/solr/card-query-filters.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+// the card to show all query filters.
+v-card( v-if="filters" class="mb-3")
+  v-list
+    v-subheader Filters
+  
+    v-list-item-group
+      // @click="$emit('bucket-select', facet.name, bucket.value)"
+      v-list-item(
+        v-for="(filter, i) in filters"
+        :key="'filter-' + i"
+      )
+        v-list-item-content {{filter.replace(":", ": ")}}
+        v-list-item-action
+          v-list-item-action-text X
+</template>
+
+<script>
+
+export default {
+
+    name: "query-filters",
+
+    props: ["filters"],
+
+    // using computed for dynamic data.
+    computed: {
+    }
+}
+</script>
