b6672e5fd1cccd52db0c96a7b2b3ca1d0e95d40f
Sean Chen <sean.chen@leocorn.com>
2020-01-09 10:52:00 -0500

    update to have expansion panels as the root element

--- pages/solr/card-facet-buckets.vue
+++ pages/solr/card-facet-buckets.vue
@@ -1,19 +1,22 @@
 <template lang="pug">
-// the preview card.
-v-expansion-panel(
-)
-  v-expansion-panel-header {{facetLabel}} - {{facetBuckets.length}}
-
-  v-expansion-panel-content
-    v-list
-      v-list-item-group
-        v-list-item(
-          v-for="(bucket, index) in facetBuckets"
-          :key="index"
-        )
-          v-list-item-content {{bucket.value}}
-          v-list-item-action
-            v-list-item-action-text {{bucket.count}}
+// the card to list factes.
+v-expansion-panels
+  v-expansion-panel(
+    v-for="(facet, index) in facets"
+    :key="'facet-' + index"
+  )
+    v-expansion-panel-header {{facet.name}} - {{facet.buckets.length}}
+  
+    v-expansion-panel-content
+      v-list
+        v-list-item-group
+          v-list-item(
+            v-for="(bucket, index) in facet.buckets"
+            :key="index"
+          )
+            v-list-item-content {{bucket.value}}
+            v-list-item-action
+              v-list-item-action-text {{bucket.count}}
 </template>
 
 <script>
@@ -22,19 +25,10 @@ export default {
 
     name: "facet-buckets",
 
-    props: ["facet"],
+    props: ["facets"],
 
     // using computed for dynamic data.
     computed: {
-        // the facet label.
-        facetLabel() {
-            return this.facet.name;
-        },
-
-        // facet buckets.
-        facetBuckets() {
-            return this.facet.buckets;
-        }
     }
 }
 </script>
