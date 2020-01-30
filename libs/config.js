79eb5ea38a2d90899fc1117b01c41fea1b873fa6
Sean Chen <sean.chen@leocorn.com>
2020-01-30 15:15:29 -0500

    a dummy local config file

--- libs/config.js
+++ libs/config.js
@@ -1,11 +1,24 @@
 /**
  * some configurations.
  */
+
+import local from '@/libs/local';
+
 let config = {
 
     root: 'http://www.google.com/api',
     profileName: 'profile.json'
 };
+
+try {
+    // the first arg is the target Object,
+    // all other objects are source objects.
+    // The properties in source Object will override
+    // the properties in target object.
+    config = Object.assign(config, local);
+} catch (e) {
+    console.log('no local config');
+}
 
 // export
 export default config;
