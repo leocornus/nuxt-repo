9cd2c0108b924a49bf0de3f163144cce3497b4a5
Sean Chen <sean.chen@leocorn.com>
2020-01-31 15:18:38 -0500

    update to use use a layout

--- pages/pugs/index.vue
+++ pages/pugs/index.vue
@@ -1,18 +1,37 @@
 <template lang="pug">
-  h1.red Hello {{ name }}!
+v-container( grid-list-xs )
+  h1 Hello {{ name }}!
+  h3 Client IP: {{clientIp}}
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+
+    auth: false,
+
+    layout: 'vuetify',
+
     data() {
         return {
             name: 'Pug World'
         }
+    },
+
+    asyncData() {
+
+        return axios.get('https://www.cloudflare.com/cdn-cgi/trace')
+            .then(function(res) {
+                //console.log(res);
+                let all = res.data.split("\n")[2];
+                return {clientIp: all.split("=")[1]};
+            });
     }
 }
 </script>
 
 <style lang="sass">
 .red
-  color: red
+  //color: red
 </style>
