997786f432972b8d53f77d3562485fdaa448674f
Sean Chen <sean.chen@leocorn.com>
2020-02-14 09:52:16 -0500

    add the stats card component

---
+++ pages/solr/card-stats.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+// the card to show all query filters.
+v-card( v-if="stats" class="mb-3")
+  v-list.blue.lighten-1
+    v-subheader Stats
+    v-list-item-group
+      v-list-item(
+        v-for="(key, i) in Object.keys(stats)"
+        :key="'stats-' + i"
+      )
+        v-list-item-content {{key + "=" + stats[key]}}
+</template>
+
+<script>
+
+export default {
+
+    name: "stats",
+
+    props: ["stats"],
+
+    // using computed for dynamic data.
+    computed: {
+    }
+}
+</script>
