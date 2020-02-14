133ea605d0856be685da430b3b3876f9ec08061c
Sean Chen <sean.chen@leocorn.com>
2020-02-14 10:47:25 -0500

    using data table for the stats component

--- pages/solr/card-stats.vue
+++ pages/solr/card-stats.vue
@@ -1,14 +1,22 @@
 <template lang="pug">
 // the card to show all query filters.
 v-card( v-if="stats" class="mb-3")
-  v-list.blue.lighten-1
-    v-subheader Stats
-    v-list-item-group
-      v-list-item(
-        v-for="(key, i) in Object.keys(stats)"
-        :key="'stats-' + i"
-      )
-        v-list-item-content {{key + "=" + stats[key]}}
+  // using v-data-table
+  v-data-table(
+    :headers="headers"
+    :items="statsArray"
+    hide-default-header
+    hide-default-footer
+  )
+  // using v-list.
+  //v-list.blue.lighten-1
+  //  v-subheader Stats
+  //  v-list-item-group
+  //    v-list-item(
+  //      v-for="(key, i) in Object.keys(stats)"
+  //      :key="'stats-' + i"
+  //    )
+  //      v-list-item-content {{key + "=" + stats[key]}}
 </template>
 
 <script>
@@ -19,8 +27,27 @@ export default {
 
     props: ["stats"],
 
+    data() {
+        return {
+            headers: [
+                { text: "name", value: "name" },
+                { text: "value", value: "value" },
+            ]
+        }
+    },
+
     // using computed for dynamic data.
     computed: {
+
+        statsArray: function() {
+
+            return Object.keys(this.stats).map( key => {
+                return {
+                    name: key,
+                    value: this.stats[key]
+                }
+            });
+        }
     }
 }
 </script>
