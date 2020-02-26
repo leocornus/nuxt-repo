7696ad38d9c86930574e8c2f3c9defa1b038d6f3
Sean Chen <sean.chen@leocorn.com>
2020-02-26 13:05:21 -0500

    get started time window.

---
+++ pages/solr/card-time-window.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+// the card to show time window.
+v-card( class="mb-3")
+  h4 Time Window
+  // group of buttons.
+</template>
+
+<script>
+
+export default {
+
+    name: "time-window",
+
+    props: [],
+
+    data() {
+        return {
+        }
+    },
+
+    // using computed for dynamic data.
+    computed: {
+
+    },
+    
+    methods: {
+
+    }
+}
+</script>
