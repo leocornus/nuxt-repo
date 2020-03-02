4ac8f7525bd61c78ad8a3c9cc9d39a32f3c8dde1
Sean Chen <sean.chen@leocorn.com>
2020-03-01 23:53:45 -0500

    play around to use expansion panel.

--- pages/solr/card-time-window.vue
+++ pages/solr/card-time-window.vue
@@ -1,54 +1,57 @@
 <template lang="pug">
 // the card to show time window.
-v-card( class="mb-3" dense )
-  v-card-subtitle Time Window
-  // group of buttons.
-  v-card-text
-    // relative time window
-    v-btn-toggle(
-      v-if="windowType === 'relative'"
-      v-model="toggleExclusive"
-      active-class="primary--text"
-      mandatory
-      dense
-    )
-      v-btn(
-        @click = "$emit('relative-select', 0)"
-      ) All
-      v-btn(
-        @click = "$emit('relative-select', 1)"
-      ) Today
-      v-btn(
-        @click = "$emit('relative-select', 7)"
-      ) 7 Days
-      v-btn(
-        @click = "$emit('relative-select', 30)"
-      ) 30 Days
-      v-btn(
-        @click = "$emit('relative-select', 60)"
-      ) 2 Monthes
-      v-btn(
-        @click = "$emit('relative-select', 90)"
-      ) 3 Monthes
-
-    // time window type.
-    v-chip-group(
-      v-model="windowType"
-      mandatory
-      active-class="deep-purple--text text--accent-4"
-    )
-      v-chip(
-        value="relative"
-        @click="$emit('time-window-type', 'relative')"
-      ) Relative
-      v-chip(
-        value="absolute"
-        @click="$emit('time-window-type', 'absolute')"
-      ) Absolute
-      v-chip(
-        value="since"
-        @click="$emit('time-window-type', 'since')"
-      ) Since
+v-expansion-panel
+  v-expansion-panel-header Time Window
+  v-expansion-panel-content
+    v-card( class="mb-3" dense )
+      //v-card-subtitle Time Window
+      // group of buttons.
+      v-card-text
+        // relative time window
+        v-btn-toggle(
+          v-if="windowType === 'relative'"
+          v-model="toggleExclusive"
+          active-class="primary--text"
+          mandatory
+          dense
+        )
+          v-btn(
+            @click = "$emit('relative-select', 0)"
+          ) All
+          v-btn(
+            @click = "$emit('relative-select', 1)"
+          ) Today
+          v-btn(
+            @click = "$emit('relative-select', 7)"
+          ) 7 Days
+          v-btn(
+            @click = "$emit('relative-select', 30)"
+          ) 30 Days
+          v-btn(
+            @click = "$emit('relative-select', 60)"
+          ) 2 Monthes
+          v-btn(
+            @click = "$emit('relative-select', 90)"
+          ) 3 Monthes
+    
+        // time window type.
+        v-chip-group(
+          v-model="windowType"
+          mandatory
+          active-class="deep-purple--text text--accent-4"
+        )
+          v-chip(
+            value="relative"
+            @click="$emit('time-window-type', 'relative')"
+          ) Relative
+          v-chip(
+            value="absolute"
+            @click="$emit('time-window-type', 'absolute')"
+          ) Absolute
+          v-chip(
+            value="since"
+            @click="$emit('time-window-type', 'since')"
+          ) Since
 </template>
 
 <script>
