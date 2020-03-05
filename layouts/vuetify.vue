d81e13a3d3b1ff5a61c5f4ac1c705637a1ff89f1
Sean Chen <sean.chen@leocorn.com>
2020-03-05 08:56:01 -0500

    add link

--- layouts/vuetify.vue
+++ layouts/vuetify.vue
@@ -50,7 +50,7 @@ v-app
   v-footer(color="indigo" app padless)
     v-layout(justify-center row wrap)
       v-flex(py-3 text-center white--text xs12)
-        | &copy;{{new Date().getFullYear()}} - <strong>Leocornus LTD.</strong>
+        | &copy;{{new Date().getFullYear()}} - <strong><a href="https://leocorn.com">Leocornus LTD.</a></strong>
 </template>
 
 <script>
