453d651a296a0bd86c8678d45f85990fede5312d
Sean Chen <sean.chen@leocorn.com>
2020-03-06 20:35:11 -0500

    get ready the game board

--- pages/games/tetris.vue
+++ pages/games/tetris.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
 v-app
-  h1 Hello Tetris!
+  div.grid
+    canvas( id="board" ).game-board
+    div.right-column
+      div
+        h1 TETRIS
+        p Score:
+          span( id="score" ) 0
+        p Lines:
+          span( id="lines" ) 0
+        p Level:
+          span( id="level" ) 0
+  v-btn Play
 </template>
 
 <script>
@@ -9,3 +20,27 @@ export default {
     auth: false
 }
 </script>
+
+<style lang="css">
+.grid {
+  display: grid;
+  grid-template-columns: 320px 200px;
+}
+
+.right-column {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.game-board {
+  border: solid 2px;
+}
+
+.play-button {
+  background-color: #4caf50;
+  font-size: 16px;
+  padding: 15px 30px;
+  cursor: pointer;
+}
+</style>
