58e1ba79e867af44ea77c77eadc5de77a5a1e0fa
Sean Chen <sean.chen@leocorn.com>
2020-03-09 13:34:33 -0400

    create a javascript class for tetromino piece

--- pages/games/tetris.vue
+++ pages/games/tetris.vue
@@ -15,6 +15,51 @@ v-app
 </template>
 
 <script>
+/**
+ * the piece class.
+ */
+class Piece {
+  x;
+  y;
+  color;
+  shape;
+  ctx;
+  
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.spawn();
+  }
+  
+  spawn() {
+    this.color = 'blue';
+    this.shape = [
+      [2, 0, 0], 
+      [2, 2, 2], 
+      [0, 0, 0]
+    ];
+    
+    // Starting position.
+    this.x = 3;
+    this.y = 0;
+  }
+
+    draw() {
+        this.ctx.fillStyle = this.color;
+  
+        this.shape.forEach((row, y) => {
+            row.forEach((value, x) => {
+              // this.x, this.y gives the left upper position of the shape
+              // x, y gives the position of the block in the shape
+              // this.x + x is then the position of the block on the board
+              if (value > 0) {
+
+                  this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+              }
+            });
+        }); 
+    }
+}
+
 export default {
 
     auth: false,
@@ -47,8 +92,12 @@ export default {
         play() {
 
             this.grid = this.getEmptyBoard();
-
             console.table(this.grid);
+
+            let piece = new Piece(this.ctx);
+            console.log(piece);
+
+            piece.draw();
         },
 
         getEmptyBoard() {
