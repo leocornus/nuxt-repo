5bee1cfdb7325ae1d815c370d85fd23196496c25
Sean Chen <sean.chen@leocorn.com>
2020-03-21 17:40:20 -0400

    get started.

---
+++ pages/covid19/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  h3 Hello
+</template>
+
+<script>
+export default {
+
+    auth: false,
+
+    layout: 'vuetify'
+}
+</script>
+
+<style lang="sass">
+.red
+  //color: red
+</style>
