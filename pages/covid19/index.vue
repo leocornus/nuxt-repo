c19d30fa0e098733837bb861c321ed71424f52d1
Sean Chen <sean.chen@leocorn.com>
2020-03-22 20:22:43 -0400

    started with a data table.

--- pages/covid19/index.vue
+++ pages/covid19/index.vue
@@ -1,14 +1,51 @@
 <template lang="pug">
 v-container( grid-list-xs )
-  h3 Hello
+  v-row
+    v-col( cols="12" md="12" )
+      v-data-table(
+         :headers="headers"
+         :items="cases"
+         :items-per-page="perPage"
+         hide-default-footer
+      )
 </template>
 
 <script>
+
+import covid from '@/libs/covid19.js';
+
 export default {
 
     auth: false,
 
-    layout: 'vuetify'
+    layout: 'vuetify',
+
+    // components.
+
+    // data.
+    data() {
+        return {
+
+            // items per page.
+            perPage: 25,
+
+            cases: [],
+
+            headers: [
+                { text: "Country", value: "country_region"},
+                { text: "Confirmed", value: "confirmed"},
+                { text: "Deaths", value: "death"},
+                { text: "Recovered", value: "recovered"}
+            ]
+        };
+    },
+
+    /**
+     */
+    created() {
+
+        covid.getCases(this, 0);
+    }
 }
 </script>
 
