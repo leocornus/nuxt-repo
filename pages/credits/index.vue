526b327e879d134de53fab5e0bd0dd7030913c99
Sean Chen <sean.chen@leocorn.com>
2020-03-28 13:06:15 -0400

    get started the credits page.

---
+++ pages/credits/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  h2 Add Accomplishment
+  v-form
+    v-row(align="end")
+      v-col(cols="9")
+        v-text-field(
+          label="Accomplishement Description"
+        )
+        v-text-field(
+          label="Credit earned"
+        )
+      v-col(cols="3")
+        v-btn(color="primary") Add
+</template>
+
+<script>
+
+export default {
+
+    auth: false,
+    layout: 'vuetify',
+
+    data() {
+         return {
+         };
+    }
+}
+</script>
+
+<style lang="sass">
+</style>
