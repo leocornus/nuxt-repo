1aab70a60496c76cd3fd9923f2edec25d0ffa9b6
Sean Chen <sean.chen@leocorn.com>
2020-04-07 18:42:21 -0400

    get started the clocks page

---
+++ pages/clocks/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+v-container(grid-list-md text-center)
+  v-row
+    v-col( col="3" )
+      v-card
+        v-card-text
+          div Simple number clock
+          p {{ currentTime }}
+    v-col( col="3" )
+    v-col( col="3" )
+    v-col( col="3" )
+</template>
+
+<script>
+export default {
+
+    layout: 'vuetify',
+    name: 'clocks',
+    auth: false,
+
+    methods: {
+    }
+
+}
+</script>
