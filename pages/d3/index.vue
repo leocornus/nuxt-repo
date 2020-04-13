c959e959908d2c66eb98eefa9790f0e19bcd15fe
Sean Chen <sean.chen@leocorn.com>
2020-04-13 11:57:24 -0400

    simple line chart using D3

--- pages/d3/index.vue
+++ pages/d3/index.vue
@@ -1,32 +1,69 @@
 <template lang="pug">
 v-container(grid-list-md text-center)
-  v-row
-    v-col( col="3" )
-      v-card
-        v-card-text
-          div Simple number clock
-          p.display-1.text--primary Hello Clock
-    v-col( col="3" )
-      v-card
-        v-card-text
-          div Simple count down
-          p.display-1.text--primary Hello Timer
-    v-col( col="3" )
-    v-col( col="3" )
+  svg(
+    width="500"
+    height="270"
+  )
+    g( style="transform: translate(0, 10px)" )
+      path( :d="line" )
 </template>
 
 <script>
+import * as d3 from "d3";
+
 export default {
 
     layout: 'vuetify',
-    name: 'clocks',
+    name: 'simple-line-chart',
     auth: false,
 
     data() {
 
         return {
 
+            data: [99, 71, 78, 25, 36, 92],
+            line: ''
+
         };
+    },
+
+    mounted() {
+
+        this.calculatePath();
+    },
+
+    methods: {
+
+        getScales() {
+
+            const x = d3.scaleTime().range([0, 430]);
+            const y = d3.scaleLinear().range([210, 0]);
+            d3.axisLeft().scale(x);
+            d3.axisBottom().scale(y);
+            x.domain(d3.extent(this.data, (d, i) => i));
+            y.domain([0, d3.max(this.data, d => d)]);
+            return { x, y };
+        },
+            
+        calculatePath() {
+
+            const scale = this.getScales();
+            const path = d3.line()
+              .x((d, i) => scale.x(i))
+              .y(d => scale.y(d));
+
+            this.line = path(this.data);
+        }
     }
 }
 </script>
+
+<style lang="sass" scoped>
+svg
+  margin: 25px
+
+path
+  fill: none
+  stroke: #76BF8A
+  stroke-width: 3px
+</style>
