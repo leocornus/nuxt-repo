b2a76003264345ded8fe41bb4e54386fbc90eba4
Sean Chen <sean.chen@leocorn.com>
2020-04-13 11:15:19 -0400

    add a simple 404 page.

---
+++ pages/404.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+v-container(grid-list-md text-center)
+  h1 Page NOT exit!
+  h2 Go back to 
+    n-link(to="/") Homepage
+</template>
+
+<script>
+
+import Profile from '@/libs/profile';
+
+export default {
+
+    layout: 'vuetify',
+
+    name: "homepage",
+
+    // turn off authtication.
+    auth: false,
+}
+</script>
+
+<style lang="sass">
+</style>
