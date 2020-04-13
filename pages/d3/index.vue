74f315e8e0a77f41ed1bc0b1b4a8e4ecbbcc9740
Sean Chen <sean.chen@leocorn.com>
2020-04-13 11:07:57 -0400

    get started.

---
+++ pages/d3/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+v-container(grid-list-md text-center)
+  v-row
+    v-col( col="3" )
+      v-card
+        v-card-text
+          div Simple number clock
+          p.display-1.text--primary Hello Clock
+    v-col( col="3" )
+      v-card
+        v-card-text
+          div Simple count down
+          p.display-1.text--primary Hello Timer
+    v-col( col="3" )
+    v-col( col="3" )
+</template>
+
+<script>
+export default {
+
+    layout: 'vuetify',
+    name: 'clocks',
+    auth: false,
+
+    data() {
+
+        return {
+
+        };
+    }
+}
+</script>
