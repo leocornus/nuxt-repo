aa92dd3d7b445c875600530586e42a2d95ac3e6c
Sean Chen <sean.chen@leocorn.com>
2020-04-14 23:19:34 -0400

    fix typo, axes is the plural of axis

--- pages/d3/index.vue
+++ pages/d3/index.vue
@@ -4,7 +4,7 @@ v-container(grid-list-md text-center)
     v-list-item
       n-link( to="/d3/simple-line-chart" ) Simple Line Chart
     v-list-item
-      n-link( to="/d3/line-chart" ) Line Chart with x, y axises and simple animation
+      n-link( to="/d3/line-chart" ) Line Chart with x, y axes and simple animation
     v-list-item
       n-link( to="/d3/multi-lines-chart" ) Multiple Lines Chart
 </template>
