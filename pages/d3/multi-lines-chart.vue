03d2c708709368528909d61bc536f6b3c800cb1a
Sean Chen <sean.chen@leocorn.com>
2020-04-15 14:36:23 -0400

    get started from line chart

--- pages/d3/multi-lines-chart.vue
+++ pages/d3/multi-lines-chart.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
-v-container(grid-list-md text-center)
-  svg(
-    width="500"
-    height="570"
-  )
-    // the line chart. group of element.
-    g( transform="translate(0, 10px)" )
-      path( class="one" :d="line1" )
-        text line One
-      path( class="two" :d="line2" )
-      // x axis, at the bottom.
-      // y axis at left
+v-container(grid-list-md)
+  h1 A chart with multiple lines.
+  p Here are some features we are trying to show up:
+  ul
+    li responsive line chart.
+    li simple animation
+
+  v-row
+    v-col( cols="2" )
+      v-btn(@click="updateChart") Update
+    v-col( cols="8" )
+      div( id="linechart" )
+    v-col( cols="2" )
 </template>
 
 <script>
@@ -26,42 +27,153 @@ export default {
 
         return {
 
-            data1: [99, 71, 120, 89, 78, 25, 36, 92],
-            data2: [90, 71, 110, 89, 58, 25, 26, 92],
-            line1: '',
-            line2: '',
-
+            // set margins for the chart.
+            // margin will be used to draw axes.
+            chartMargin: {
+                top: 20,
+                right: 20,
+                bottom: 30,
+                left: 50
+            }
         };
     },
 
     mounted() {
 
-        this.calculatePath();
+        this.drawChart();
     },
 
     methods: {
 
-        getScales() {
+        /**
+         */
+        updateChart() {
 
-            const x = d3.scaleTime().range([0, 500]);
-            const y = d3.scaleLinear().range([570, 0]);
-            d3.axisLeft().scale(x);
-            d3.axisBottom().scale(y);
-            x.domain(d3.extent(this.data1, (d, i) => i));
-            y.domain([0, d3.max(this.data1, d => d)]);
-            return { x, y };
+            // clear the chart.
+            d3.selectAll("svg").remove();
+            this.drawChart();
+
         },
+
+        /**
+         * draw the line chart.
+         */
+        drawChart() {
+
+            let vm = this;
+
+            // initialize the svg element.
+            vm.initializeSvg();
+
+            // utility to parse string to time.
+            let parseTime = d3.timeParse("%Y-%m-%d");
+
+            // load initial data.
+            d3.csv("/d3/lines.csv",
+                // work on each row to make sure the data type.
+                function(aLine) {
+                    aLine.date = parseTime(aLine.date);
+                    // the leading + will make sure to convert to an integer.
+                    aLine.close = +aLine.value;
+                    return aLine;
+                }
+            ).then(function(data) {
             
-        calculatePath() {
+                console.log(data);
 
-            const scale = this.getScales();
-            const path = d3.line()
-              .x((d, i) => scale.x(i))
-              .y(d => scale.y(d));
+                // setup domain for x, y scale range, based on the live data.
+                vm.xRange.domain(d3.extent(data, function(aLine) { return aLine.date; } ));
+                vm.yRange.domain([0, d3.max(data, function(aLine) { return aLine.close; }) + 100]);
 
-            this.line1 = path(this.data1);
-            this.line2 = path(this.data2);
-        }
+                // add the x axis at the bottom.
+                vm.linesGroup.append("g")
+                  .attr("transform", "translate(0," + vm.chartHeight + ")")
+                  .call(d3.axisBottom(vm.xRange));
+                  // this is to remove the line on X axis.
+                  //.select(".domain")
+                  //.remove();
+
+                // add the y axis at the left.
+                vm.linesGroup.append("g")
+                  .call(d3.axisLeft(vm.yRange))
+                  // add text label.
+                  .append("text")
+                    .attr("fill", "#000")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", "0.71em")
+                    .attr("text-anchor", "end")
+                    .text("Price ($)");
+
+                // add path for lines.
+                vm.path = vm.linesGroup.append("path")
+                  .datum(data)
+                  .attr("fill", "none")
+                  .attr("stroke", "steelblue")
+                  .attr("stroke-linejoin", "round")
+                  .attr("stroke-linecap", "round")
+                  .attr("stroke-width", 1.5)
+                  .attr("d", vm.lineFunc);
+
+                // get total nodes for transition.
+                let totalLength = vm.path.node().getTotalLength();
+                //console.log("Total Length: ", totalLength);
+                //console.log("The path selection: ", vm.path.node());
+                vm.path
+                  .attr("stroke-dasharray", totalLength + " " + totalLength)
+                  // this is the initial value
+                  .attr("stroke-dashoffset", totalLength)
+                  .transition()
+                    .duration(5000)
+                    .ease(d3.easeLinear)
+                    .attr("stroke-dashoffset", 0); // this is the final value.
+            });
+        },
+
+        /**
+         * get ready the SVG element.
+         */
+        initializeSvg() {
+
+            let vm = this;
+
+            // create svg, using 100% of the room!
+            vm.svg = d3.select('#linechart').append("svg")
+                .attr("width", "100%")
+                //.attr("height", "100%");
+                //.attr("width", 500)
+                .attr("height", 480);
+                //.call(vm.responsivefy);
+
+            //console.log(vm.svg.attr('width'));
+            //let parentNode = d3.select(d3.select('#linechart').node().parentNode);
+            //console.log(parseInt(parentNode.style('width')));
+            //console.log("middle col: ", parentNode.node().getBoundingClientRect());
+            //console.log("svg DOMRect: ", vm.svg.node().getBoundingClientRect());
+            //console.log("svg: ", vm.svg.node().getBBox());
+            //console.log(vm.chartMargin);
+            
+            // calculate the dimention of the chart.
+            // +vm.svg.attr("innerWidth")
+            const svgRect = vm.svg.node().getBoundingClientRect();
+            vm.chartWidth = svgRect.width - vm.chartMargin.left - vm.chartMargin.right;
+            // +vm.svg.style("height")
+            vm.chartHeight = svgRect.height - vm.chartMargin.top - vm.chartMargin.bottom;
+            console.log(vm.chartWidth, vm.chartHeight);
+
+            // the element group for lines.
+            vm.linesGroup = vm.svg.append("g")
+                .attr("transform", "translate(" + vm.chartMargin.left + "," + vm.chartMargin.top + ")");
+
+            // set scale range for x and y.
+            vm.xRange = d3.scaleTime().rangeRound([0, vm.chartWidth]);
+            vm.yRange = d3.scaleLinear().rangeRound([vm.chartHeight, 0]);
+
+            // define or set the function to draw the line.
+            vm.lineFunc = d3.line()
+                .x( function(aItem) { return vm.xRange( aItem.date ) } )
+                .y( function(aItem) { return vm.yRange( aItem.close) } );
+        },
     }
 }
 </script>
