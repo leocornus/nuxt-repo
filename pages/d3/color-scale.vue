6be17cf6d3130def4b991475fffd3b051855d27e
Sean Chen <sean.chen@leocorn.com>
2020-04-24 22:05:22 -0400

    get started the color scale page

---
+++ pages/d3/color-scale.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+v-container(grid-list-md)
+  h1 How D3 handle color scale
+  p Play around how d3 pick colors.
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+
+    layout: 'vuetify',
+    name: 'color-scale',
+    auth: false,
+
+    data() {
+        return {
+
+        };
+    },
+
+    methods: {
+    }
+}
+</script>
