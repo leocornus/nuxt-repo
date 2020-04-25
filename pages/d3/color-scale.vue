511bad9cd1e0a778c49ec87886f07c8d454545b0
Sean Chen <sean.chen@leocorn.com>
2020-04-24 23:07:31 -0400

    the category 10 scheme

--- pages/d3/color-scale.vue
+++ pages/d3/color-scale.vue
@@ -2,6 +2,17 @@
 v-container(grid-list-md)
   h1 How D3 handle color scale
   p Play around how d3 pick colors.
+
+  h2 Category10 scheme
+  p( 
+    v-for="(sentence, index) in category10Strings"
+    :key = "index"
+  )
+    v-chip(
+      v-for="(word, wi) in sentence.split(' ')"
+      :color="category10Colors(word)"
+      label
+    ) {{ word }}
 </template>
 
 <script>
@@ -16,6 +27,14 @@ export default {
     data() {
         return {
 
+            // the category10 scheme.
+            category10Colors: d3.scaleOrdinal( d3.schemeCategory10 ),
+
+            category10Strings: [
+                "A sentence with less than ten words",
+                "A sentence with exact ten words one two three four",
+                "A sentence with more than ten words one two three four"
+            ],
         };
     },
 
