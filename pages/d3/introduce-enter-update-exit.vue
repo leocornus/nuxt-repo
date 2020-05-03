c7d70e26f307c26c426901b7cdf6109b4148a167
Sean Chen <sean.chen@leocorn.com>
2020-05-03 11:06:41 -0400

    get ready to show the magic

--- pages/d3/introduce-enter-update-exit.vue
+++ pages/d3/introduce-enter-update-exit.vue
@@ -22,6 +22,76 @@ export default {
 
     layout: 'vuetify',
     name: 'enter-update-exit',
-    auth: false
+    auth: false,
+
+    data() {
+        return {
+            dataset: [15,9,34,28,59],
+            maxHeight: 100
+        };
+    },
+
+    mounted() {
+        this.update();
+    },
+
+    methods: {
+
+        update() {
+
+            let self = this;
+
+            // Update selection: Resize and position existing 
+            // DOM elements with data bound to them.
+            var selection = d3.select("#chart")
+              .selectAll(".bar").data(self.dataset)
+              .style("height", function(d){ 
+                  return d; 
+              })
+              .style("margin-top", function(d){ 
+                  return self.maxHeight - d; 
+              });
+
+            // Enter selection: Create new DOM elements for added 
+            // data items, resize and position them and attach a 
+            // mouse click handler.
+            selection.enter()
+              .append("div").attr("class", "bar")
+              .style("height", function(d){ 
+                  return d; 
+              })
+              .style("margin-top", function(d){ 
+                  return self.maxHeight - d; 
+              })
+              .on("click", function(e, i){
+                  self.dataset.splice(i, 1);
+                  self.update();
+              });
+
+              // Exit selection: Remove elements without data from the DOM
+              selection.exit().remove();
+
+              // Print underlying data array
+              d3.select("#dataset").text(self.dataset);
+        }
+    }
 }
 </script>
+
+<style lang="css">
+.bar {
+  width: 25px;
+  margin-right: 5px;
+  background-color: #C1ADC4;
+  float: left;
+  cursor: pointer;
+}
+  
+.bar:last-child {
+  margin-right: 0;
+}
+
+.bar:hover{
+  background-color: #927A94;
+}
+</style>
