a12c370665855dadafee1d00b265d31d55d9f882
Sean Chen <sean.chen@leocorn.com>
2020-05-09 16:56:58 -0400

    add autocomplete for all available pages

--- pages/d3/index.vue
+++ pages/d3/index.vue
@@ -1,5 +1,16 @@
 <template lang="pug">
 v-container(grid-list-md text-center)
+  v-row
+    v-col( cols="12" )
+      v-autocomplete(
+        v-model="selectedPage"
+        label="Pick an example page"
+        dense
+        solo
+        :items="d3Pages"
+        clearable
+        prepend-icon="mdi-file-find"
+      )
   v-list
     v-list-item
       n-link( to="/d3/introduce-enter-update-exit" ) D3 introduction update enter exit
@@ -16,11 +27,46 @@ v-container(grid-list-md text-center)
 </template>
 
 <script>
+
+import * as d3 from 'd3';
+
 export default {
 
     layout: 'vuetify',
     name: 'd3-demo',
     auth: false,
+
+    data() {
+        return {
+            selectedPage: ''
+        }
+    },
+
+    watch: {
+
+        selectedPage: function(page) {
+
+            //console.log('selected page:', page);
+            window.location.href=page.split("/")[1];
+        }
+    },
+
+    /**
+     * using async data for pages list.
+     */
+    async asyncData() {
+
+        const list = await d3.tsv('/pages-list.csv')
+        //console.log(list);
+        return {
+            d3Pages: list.map( aPage => {
+                return {
+                    text: aPage.page_path.split(".")[0],
+                    value: aPage.page_path.split(".")[0]
+                };
+            })
+        };
+    }
 }
 </script>
 
