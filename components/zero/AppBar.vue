df36fee3d4f41ff8be2320eea65694742d9365ef
Sean Chen <sean.chen@leocorn.com>
2020-06-03 16:11:10 -0400

    add more items on the app bar

--- components/zero/AppBar.vue
+++ components/zero/AppBar.vue
@@ -14,6 +14,31 @@ div
       max-width="52"
       width="100%"
     )
+    v-img(
+      :src="require('@/assets/zero-logo-light.svg')"
+      contain
+      max-width="128"
+      width="100%"
+    )
+
+    v-spacer
+
+    div
+      v-tabs(
+        optional
+        class="hidden-sm-and-down"
+      )
+        v-tab(
+          v-for="(name, i) in items"
+          :key="i"
+          :to="{ name }"
+          :exact="name === 'Home'"
+          :ripple="false"
+          active-class="text--primary"
+          class="font-weight-bold"
+          min-width="96"
+          text
+        ) {{ name }}
 </template>
 
 <script>
@@ -24,9 +49,9 @@ div
       drawer: null,
       items: [
         'Home',
-        'About',
-        'Contact',
-        'Pro',
+        //'About',
+        //'Contact',
+        //'Pro',
       ],
     }),
   }
