87fc054788cbf612b818997d5d5e53a761671d30
Sean Chen <sean.chen@leocorn.com>
2020-06-03 15:13:21 -0400

    get started the demo page for zero layout

---
+++ pages/zero-theme/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+v-container
+  h1 Hello Zero
+</template>
+
+<script>
+export default {
+
+    layout: 'zero',
+    auth: false,
+    name: 'Zero Theme demo page'
+}
+</script>
