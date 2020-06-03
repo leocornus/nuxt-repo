a5e4b6c624e8e9de79b0d58926814a7558339f23
Sean Chen <sean.chen@leocorn.com>
2020-06-03 15:28:56 -0400

    introduce the app bar component

---
+++ components/zero/AppBar.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+div
+  v-app-bar(
+    id="home-app-bar"
+    app
+    color="white"
+    elevation="1"
+    height="80"
+  )
+    v-img(
+      :src="require('@/assets/logo.svg')"
+      class="mr-3 hidden-xs-only"
+      contain
+      max-width="52"
+      width="100%"
+    )
+</template>
+
+<script>
+  export default {
+    name: 'ZeroAppBar',
+
+    data: () => ({
+      drawer: null,
+      items: [
+        'Home',
+        'About',
+        'Contact',
+        'Pro',
+      ],
+    }),
+  }
+</script>
