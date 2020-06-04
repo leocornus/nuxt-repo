eb76deff7998f40e7d2cb9b95abc2619ee7b075a
Sean Chen <sean.chen@leocorn.com>
2020-06-04 12:45:51 -0400

    add drawer for sm screen.

---
+++ components/zero/Drawer.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+// the drawer for sm screen.
+v-navigation-drawer(
+  top 
+  color="transparent"
+  fixed
+  height="auto"
+  overlay-color="secondary"
+  overlay-opacity=".8"
+  temporary
+  v-bind="$attrs"
+  v-on="$listeners"
+)
+  v-list(
+    color="white"
+    shaped
+  )
+    v-list-item(
+      v-for="name in items"
+      :key="name"
+      :to="{ name }"
+      :exact="name === 'Home'"
+      color="primary"
+    )
+      v-list-item-content
+        v-list-item-title( v-text="name" )
+</template>
+<script>
+export default{
+
+    name: 'ZeroDrawer',
+
+    props: {
+        items: {
+            type: Array,
+            default: () => ([]),
+        }
+    }
+}
+</script>
