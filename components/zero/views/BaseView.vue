edef767f704185a61b8920c5150c2a284315c86a
Sean Chen <sean.chen@leocorn.com>
2020-06-13 18:14:13 -0400

    update the component id with zero- prefix

---
+++ components/zero/views/BaseView.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+section( :id="id" )
+  v-row( no-gutters )
+    v-col( cols="12" )
+      component(
+        :is="`zero-section-${section}`"
+        v-for="section in sections"
+        :key="section"
+      )
+</template>
+
+<script>
+export default {
+    name: 'ZeroBaseView',
+
+    props: {
+        id: {
+          type: String,
+          default: 'view',
+        },
+    },
+
+    data: () => ({ sections: [] }),
+}
+</script>
