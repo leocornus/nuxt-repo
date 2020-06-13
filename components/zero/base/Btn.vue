5afca2cf668d4028ea9ae9a2c66070ad5aa1a57b
Sean Chen <sean.chen@leocorn.com>
2020-06-13 18:17:14 -0400

    the base button for Zero theme.

---
+++ components/zero/base/Btn.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  v-btn(
+    :color="color"
+    :depressed="depressed"
+    :min-width="minWidth"
+    :tile="tile"
+    class="font-weight-bold"
+    x-large
+    v-bind="$attrs"
+    v-on="$listeners"
+  ) 
+    slot
+</template>
+
+<script>
+export default {
+    name: 'ZeroBaseBtn',
+
+    props: {
+        color: {
+          type: String,
+          default: 'primary',
+        },
+        depressed: {
+          type: Boolean,
+          default: true,
+        },
+        minWidth: {
+          type: [Number, String],
+          default: 164,
+        },
+        tile: {
+          type: Boolean,
+          default: true,
+        },
+    },
+}
+</script>
