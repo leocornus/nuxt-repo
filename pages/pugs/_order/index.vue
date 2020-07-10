cf27b3e5e5a5e089607a1b49384dcfcc3673325f
Sean Chen <sean.chen@leocorn.com>
2020-07-10 11:39:19 -0400

    add the order parameter

---
+++ pages/pugs/_order/index.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  ul
+    li Order ID: {{ $route.params.order}}
+</template>
+
+<script>
+
+export default {
+
+    name: 'DynamicOrder',
+
+    auth: false,
+
+    layout: 'vuetify',
+
+    data() {
+        return {
+        }
+    }
+
+}
+</script>
+
+<style lang="sass">
+.red
+  //color: red
+</style>
