5608990f6b4c3919e425ea396154ac8e0c80a102
Sean Chen <sean.chen@leocorn.com>
2020-07-10 11:48:41 -0400

    test the 2 level dynamic routing

---
+++ pages/pugs/_order/_cat/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  ul
+    li Order ID: {{ $route.params.order }}
+    li Category: {{ $route.params.cat }}
+</template>
+
+<script>
+
+export default {
+
+    name: 'DynamicOrder',
+
+    auth: false,
+
+    layout: 'vuetify',
+
+    data() {
+        return {
+        }
+    }
+
+}
+</script>
