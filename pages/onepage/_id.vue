010f9a7c4edf7a5114d09d74be4273208f59d174
Sean Chen <sean.chen@leocorn.com>
2020-07-10 11:12:53 -0400

    test the dynamic route

---
+++ pages/onepage/_id.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  h1 Hello {{ $route.params.id }}!
+</template>
+
+<script>
+
+export default {
+
+    name: 'DynamicId',
+
+    auth: false,
+
+    layout: 'vuetify',
+
+    data() {
+        return {
+        }
+    }
+
+}
+</script>
+
+<style lang="sass">
+.red
+  //color: red
+</style>
