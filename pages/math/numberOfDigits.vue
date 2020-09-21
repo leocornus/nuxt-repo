9b420e03da1302fe6f842bfc40a57d50af1a6961
Sean Chen <sean.chen@leocorn.com>
2020-09-21 15:43:06 -0400

    get started the number of digits problem

---
+++ pages/math/numberOfDigits.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  h1 Number of digits problem
+
+  v-layout( row wrap )
+    v-flex( xs12 sm4 )
+      v-toolbar( color="indigo" dark dense light )
+        v-toolbar-title Number options
+      v-card
+        v-card-title( problem-title )
+          v-row
+            v-text-field( 
+              id='from-number'
+              autocomplete="off"
+              v-model='fromNumber' 
+              label="From number:"
+              :loading='loading'
+              clearable
+            )
+            v-text-field( 
+              id='to-number'
+              autocomplete="off"
+              v-model='toNumber' 
+              label="To number:"
+              :loading='loading'
+              clearable
+            )
+            v-text-field( 
+              id='digit-number'
+              autocomplete="off"
+              v-model='digitNumber'
+              label="Looking for digit:"
+              :loading='loading'
+              clearable
+            )
+        v-card-text
+          strong( :class="msgColor" )
+            |{{ resultMessage }}
+</template>
+
+<script>
+
+export default {
+    auth: false,
+
+    layout: 'vuetify',
+
+    name: 'number-of-digit',
+
+    data() {
+        return {
+            fromNumber: null,
+            toNumber: null,
+            digitNumber: null,
+
+            msgColor: "primary--text",
+        };
+    },
+
+    computed: {
+
+        resultMessage: function() {
+
+            if(this.fromNumber === null || this.fromNumber === "") {
+
+                this.msgColor = "primary--text";
+                this.factorDisplay = "d-none";
+                return "Waiting for the numbers...";
+            } else {
+
+            }
+        }
+    },
+}
+</script>
