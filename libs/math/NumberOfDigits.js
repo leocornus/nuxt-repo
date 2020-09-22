bcd043ae308d59e78dcd45fa008515802c70eac6
Sean Chen <sean.chen@leocorn.com>
2020-09-21 21:20:36 -0400

    create the utility lib for number of digits problem

---
+++ libs/math/NumberOfDigits.js
@@ -0,0 +1,27 @@
+/**
+ * utility lib for solving number of digits problem.
+ */
+
+let nod = {
+
+    /**
+     * calculate number of digits.
+     */
+    calcNumberOfDigits: function( fromNumber, toNumber, digit ) {
+
+        let total = 0;
+
+        for( let number = fromNumber; number <= toNumber; number ++) {
+
+            // get the string format off the number.
+            const numberStr = number.toString();
+            for( let i = 0; i < numberStr.length; i++ ) {
+                total += numberStr.charAt( i ) === digit.toString() ? 1 : 0;
+            }
+        }
+
+        return total;
+    }
+};
+
+export default nod;
