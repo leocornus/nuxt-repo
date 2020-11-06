141266747e6d3bc258a6a7683fd4b50765f9212e
Sean Chen <sean.chen@leocorn.com>
2020-11-06 18:55:12 -0500

    add more comments to help read

--- pages/onepage/_id.vue
+++ pages/onepage/_id.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
 v-container( grid-list-xs )
+  // the id could be access in object $route.params
   h1 Hello {{ $route.params.id }}!
+  h2 hello again {{ theId() }}
 </template>
 
 <script>
-
+/**
+ * test the dynamic routes for the given id.
+ * - https://nuxtjs.org/docs/2.x/features/file-system-routing
+ */
 export default {
 
     name: 'DynamicId',
@@ -15,6 +20,15 @@ export default {
 
     data() {
         return {
+        };
+    },
+
+    methods: {
+
+        theId() {
+
+            // using this to access the id parameter
+            return this.$route.params.id;
         }
     }
 
