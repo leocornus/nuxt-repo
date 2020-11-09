3c2e53a672356b9a43b4a87a43cba0af076221b0
Sean Chen <sean.chen@leocorn.com>
2020-11-08 21:11:53 -0500

    get started the demo page to show items in a category

---
+++ pages/demo/_cat/index.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  v-card
+</template>
+
+<script>
+
+export default {
+
+    name: 'DemoCategory',
+    auth: false,
+    layout: 'vuetify',
+
+    data() {
+
+        return {
+
+        };
+    },
+
+    methods: {
+
+        /**
+         */
+        hello() {
+        }
+    }
+}
+</script>
