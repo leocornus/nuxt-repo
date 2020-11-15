9fb0f56fc785a49a9840a93dfa0b9f37a2845b5e
Sean Chen <sean.chen@leocorn.com>
2020-11-15 09:20:24 -0500

    setup the demo page for JSONEditor

---
+++ pages/datainspector/index.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+v-container
+  v-jsoneditor(
+    v-model="json"
+	:options="options"
+  )
+  p {{ json }}
+</template>
+
+<script>
+
+import JSONEditor from '@/components/jsoneditor';
+
+export default {
+    
+	name: 'DemoJSONEditor',
+	auth: false,
+	layout: 'vuetify',
+
+	components: {
+	    'v-jsoneditor': JSONEditor
+	},
+
+	data() {
+	    return {
+		    json: {'test': "test value"},
+			options: {
+			    mode: 'code'
+			}
+		};
+	}
+}
+</script>
