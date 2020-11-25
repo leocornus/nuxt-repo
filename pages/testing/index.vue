6e531eed17cb305b2bedabe945ce9fb5d5086653
Sean Chen <sean.chen@leocorn.com>
2020-11-25 14:50:48 -0500

    the testing index page.

---
+++ pages/testing/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+v-container(grid-list-md text-center)
+  v-row
+    v-col( col="3" )
+      v-card
+        v-card-text
+          nuxt-link( to="/testing/objectAssign" ) Object.assign
+    v-col( col="3" )
+      v-card
+        v-card-text
+    v-col( col="3" )
+    v-col( col="3" )
+</template>
+
+<script>
+export default {
+
+    layout: 'vuetify',
+    name: 'testing',
+    auth: false
+}
+</script>
