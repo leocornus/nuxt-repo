e5b1bb02d2de4361cbff0c575daeaab828f64a0e
Sean Chen <sean.chen@leocorn.com>
2020-11-26 23:50:40 -0500

    quick test to load all files in current folder.

--- pages/math/index.vue
+++ pages/math/index.vue
@@ -9,15 +9,31 @@ v-container(grid-list-md text-center)
       v-card
         v-card-text
           nuxt-link( to="/math/numberOfDigits" ) Number of Digits
-    v-col( col="3" )
+    v-col( col="3" ) {{ pages }}
     v-col( col="3" )
 </template>
 
 <script>
+
 export default {
 
     layout: 'vuetify',
     name: 'MathIndex',
-    auth: false
+    auth: false,
+
+    methods: {
+
+        pagesList: function() {
+
+        }
+    },
+
+    beforeMount() {
+
+        // load all files in current folder
+        let pages = require.context('./', true, /\.vue$/);
+
+        this.pages = pages.keys();
+    },
 }
 </script>
