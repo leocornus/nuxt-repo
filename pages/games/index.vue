bd17a17d94725e9198415f7d58e3de0dd4499d02
Sean Chen <sean.chen@leocorn.com>
2020-12-02 00:08:18 -0500

    add index page for games

---
+++ pages/games/index.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+v-container(grid-list-md text-center)
+  page-index( :pages='pages' )
+</template>
+
+<script>
+
+import PageIndex from '@/components/PageIndex';
+
+export default {
+
+    layout: 'vuetify',
+    name: "GameIndex",
+    auth: false,
+
+    components: {
+        'page-index': PageIndex
+    },
+
+    /**
+     * before the page loaded.
+     */
+    beforeMount() {
+
+        // load all files in current folder
+        let pages = require.context('./', true, /\.vue$/);
+        //console.log(pages);
+
+        // using reduce to do filter and map at the same time.
+        this.pages = pages.keys().reduce( (result, page) => {
+
+            // skip the index page.
+            if( page !== './index.vue' )
+                result.push( page
+                    .replace(/\.vue/, '')
+                    .replace(/^\.\//, '') );
+
+            // need return the result.
+            return result;
+        }, [] );
+    },
+}
+</script>
