ecf100e720633c7934a82285d36549c8ca0a5158
Sean Chen <sean.chen@leocorn.com>
2020-12-21 22:32:59 -0500

    add buttons to test Singleton class

--- pages/core/singleton.vue
+++ pages/core/singleton.vue
@@ -1,6 +1,19 @@
 <template lang="pug">
 v-container( grid-list-xs )
   h1 Test Singleton Class
+  v-btn(
+    color="primary"
+    @click="handleMethodOne"
+  ) Method One
+  h3 Method One Output: 
+    v-span {{ methodOneOutput }}
+
+  v-btn(
+    color="primary"
+    @click="handleMethodTwo"
+  ) Method Two
+  h3 Method Two Output: 
+    v-span {{ methodTwoOutput }}
 </template>
 
 <script>
@@ -11,5 +24,26 @@ export default {
     name: 'core-singleton',
     auth: false,
     layout: 'vuetify',
+
+    data: function() {
+        return {
+
+            methodOneOutput: '',
+            methodTwoOutput: ''
+        }
+    },
+
+    methods: {
+
+        handleMethodOne: function() {
+
+            this.methodOneOutput = Singleton.methodOne();
+        },
+
+        handleMethodTwo: function() {
+
+            this.methodTwoOutput = Singleton.methodTwo();
+        },
+    }
 };
 </script>
