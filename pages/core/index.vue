eac899202d91416893771beb4bfab11290175cbe
Sean Chen <sean.chen@leocorn.com>
2020-12-21 22:20:08 -0500

    get started the page for testing the singleton class

---
+++ pages/core/index.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+v-container(grid-list-md text-center)
+  page-index( :pages='pages' )
+</template>
+
+<script>
+
+import PageIndex from '@/components/PageIndex';
+
+export default {
+
+    layout: 'vuetify',
+    name: 'CoreIndex',
+    auth: false,
+
+    components: {
+        'page-index': PageIndex
+    },
+
+    methods: {
+
+    },
+
+    /**
+     * before the page loaded.
+     */
+    beforeMount() {
+
+        // load all files in current folder
+        let pages = require.context('./', true, /\.vue$/);
+        //console.log(pages);
+
+        // using reduce to do filter and map at the same time.
+        this.pages = pages.keys().reduce( (result, page) => {
+
+            // skip the index page.
+            if( page !== './index.vue' )
+                result.push( page
+                    .replace(/\.vue/, '')
+                    .replace(/^\.\//, '') );
+
+            // need return the result.
+            return result;
+        }, [] );
+    },
+}
+</script>
---
+++ pages/core/singleton.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+v-container( grid-list-xs )
+  h1 Test Singleton Class
+</template>
+
+<script>
+import Singleton from '@/libs/corejs/singleton';
+
+export default {
+
+    name: 'core-singleton',
+    auth: false,
+    layout: 'vuetify',
+};
+</script>
