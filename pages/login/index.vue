4f0a0bbbec2d320e716485c4e6626c48d4cee930
Sean Chen <sean.chen@leocorn.com>
2021-02-11 15:10:43 -0500

    update the login page.

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,8 +1,16 @@
 <template lang="pug">
 v-container(grid-list-md text-center)
-  h2 Login
-  hr
-  v-btn(@click="login") Login Auth0
+
+  h1.mt-12 Login Required!
+  v-btn(
+    @click="login"
+    color="primary"
+    x-large
+  ).mt-10
+    v-icon(
+      left
+    ) mdi-login
+    | Login to Leocornus Sandbox
 
   v-chip(
     v-if="$auth.loggedIn"
