7d8d4a4f6fd33754a86d98748a6520d5b1055bbc
Sean Chen <sean.chen@leocorn.com>
2021-02-17 00:31:39 -0500

    try the data table and textarea solution

--- components/solr/DocArticlesCard.vue
+++ components/solr/DocArticlesCard.vue
@@ -1,14 +1,41 @@
 <template lang="pug">
 // The Doc(ument) Article Tab Card is simple component to show
 // the given document in tab view.
-v-card
+v-container( fluid )
+  v-row
+    v-col(
+      cols="5"
+      max-height="800px"
+    )
+      v-data-table(
+        dense
+        hide-default-footer
+        :headers="headers"
+        :items="articleTitles"
+        :item-key="title"
+        :items-per-page="100"
+      )
+
+    v-col( cols="7" )
+      // using textarea is easier to control.
+      v-textarea(
+        rows="32"
+        label="Article Paragraph"
+        filled
+        outlined
+        :value="doc.file_content"
+      )
+
+// The tabs implementation not working well.
+// leave here for reference.
+//v-card
+  // Vertical tab will NOT need v-tabs-items.
   v-tabs(
     vertical
   )
     v-tab(
       v-for="(article, index) in doc.article_titles"
       :key="index"
-      text-align="left"
     ) {{ article }}
 
     v-tab-item(
@@ -16,15 +43,13 @@ v-card
       :key="index"
     )
       v-card( flat )
-        v-textarea(
-          rows="32"
-          label="Article Paragraph"
-          filled
-          outlined
-          :value="para"
+        v-card-text(
+          v-text="para"
         )
 </template>
+
 <script>
+
 export default {
 
     name: "DocArticlesCard",
@@ -44,12 +69,31 @@ export default {
         return {
 
             // the model to track tabs.
-            tab: null
+            tab: null,
+
+            // headers, just one column.
+            headers: [
+                { text: "Article", value:"title" }
+            ]
         }
     },
 
     computed: {
+        /**
+         */
+        articleTitles: function() {
 
+            let self = this;
+
+            let titles = self.doc["article_titles"].map( title => {
+
+                return {
+                    "title": title
+                }
+            });
+
+            return titles;
+        }
     },
 }
 </script>
